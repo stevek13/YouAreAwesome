b9ed764e9e03f5e8815053d64ce3a8b13002a937
Steve Kite <skite1@hotmail.com>
2023-10-26 08:51:53 -0700

    1.10 - Conditionals

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,44 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     
-//    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    //    @State private var messageString = "When the Genius Bar needs help, they call you!"
     @State private var messageString = ""
     
     var body: some View {
         
-            VStack {
+        VStack {
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .bold()
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(Color.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+            
+            Spacer()
+            
+            Button("Show Message") {
+                let message1 = "You Are Awesome!"
+                let message2 = "You Are Great"
                 
-                Spacer()
-               
-                Text(messageString)
-                    .font(.largeTitle)
-                    .bold()
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(Color.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-                 
-                Spacer()
-                
-                HStack() {
-                    Button("Awesome") {
-                        messageString = "You are Awesome!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    Spacer()
-                    
-                    Button("Great") {
-                        messageString = "Your are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
+                if messageString == message1 {
+                    messageString = message2
+                } else {
+                    messageString = message1
                 }
-                //            .border(.purple, width: 5)
-                .padding()
-                
-           
+            }
+            .buttonStyle(.borderedProminent)
         }
-        
+        .padding()
     }
 }
 
