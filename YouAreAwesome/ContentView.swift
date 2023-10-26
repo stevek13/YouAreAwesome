901f1266549ef6e84ad97e4aac35976b97b2337b
Steve Kite <skite1@hotmail.com>
2023-10-26 08:06:05 -0700

    End 1.8

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,16 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    @State private var messageString = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+            VStack {
+                
+                Spacer()
+               
+                Text(messageString)
+                    .font(.largeTitle)
+                    .bold()
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(Color.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                 
+                Spacer()
+                
+                HStack() {
+                    Button("Awesome") {
+                        messageString = "You are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "Your are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                //            .border(.purple, width: 5)
+                .padding()
+                
+           
         }
-        .padding()
+        
     }
 }
+
 
 #Preview {
     ContentView()
