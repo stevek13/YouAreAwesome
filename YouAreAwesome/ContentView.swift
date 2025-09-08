4eb2d58a66851ce3b240f9cd13f29ffe468688fe
Steve Kite <skite1@hotmail.com>
2025-09-08 13:46:04 -0700

    Add messages and images.  Set bounce and minimum scale factor

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,35 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var message = "I Am A Programmer!"
+    @State private var message = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
+    @State private var messageNumber = 0
+    
     var body: some View {
         
         VStack {
-            Spacer()
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
-                
+           
+
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
+                .fontWeight(.heavy)
+                .foregroundStyle(Color(.red))
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
+
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+                .animation(.default, value: imageName)
             Spacer()
-            HStack {
-                Button("Awesome!") {
-                    message = "Awesome!"
+            Button("Show Message") {
+                
+                let messages = ["You Are Awesome!",
+                                "You Are Great!",
+                                "Fabulous, That's You!",
+                                "You Are Fantastic",
+                                "You Swifty!",
+                                "You make me smile",
+                                "When The Genuis Bar Needs Help, The Call You!"]
+                
+                imageName = "image\(imageNumber)"
+
+                message = (messages[messageNumber])
+                messageNumber += 1
+                if messageNumber == messages.count {
+                    messageNumber = 0
                 }
-                Button(("Great")) {
-                    message = "Great!"
+                
+                //TODO: Update the imageName variable
+                imageNumber += 1
+                if imageNumber > 9  {
+                    imageNumber = 0
                 }
                 
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.orange)
         }
-    
+        
         .padding()
     }
     
