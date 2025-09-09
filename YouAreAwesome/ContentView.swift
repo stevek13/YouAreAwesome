573b1fd87cf9feeb989d6d15ae591f2bec8d1fbe
Steve Kite <skite1@hotmail.com>
2025-09-09 10:18:10 -0700

    Add do/catch

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,19 +6,26 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messageNumber = 0
+    @State private var imageNumber = -1
+    @State private var lastMessageNumber: Int = -1
+    @State private var lastImageNumber: Int = -1
+    @State private var soundNumber = -1
+    @State private var lastSoundNumber: Int = -1
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var soundOn: Bool = true
+    
+    let numberOfImages = 10
+    let numberOfSounds = 6
     
     var body: some View {
         
         VStack {
-           
-
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
@@ -27,13 +34,13 @@ struct ContentView: View {
                 .minimumScaleFactor(0.5)
                 .frame(height: 100)
                 .animation(.easeInOut(duration: 0.15), value: message)
-
+            Spacer()
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .aspectRatio(contentMode: .fit)
                 .clipShape(RoundedRectangle(cornerRadius: 30))
-                .shadow(radius: 30)
+                .shadow(color: .black, radius: 30)
                 .animation(.default, value: imageName)
             Spacer()
             Button("Show Message") {
@@ -41,30 +48,60 @@ struct ContentView: View {
                 let messages = ["You Are Awesome!",
                                 "You Are Great!",
                                 "Fabulous, That's You!",
-                                "You Are Fantastic",
+                                "You Are Fantastic!",
                                 "You Swifty!",
-                                "You make me smile",
-                                "When The Genuis Bar Needs Help, The Call You!"]
+                                "You make me smile!",
+                                "When The Genuis Bar Needs Help, They Call You!",
+                                "Gadzooks!  Good To See You!"]
                 
-                imageName = "image\(imageNumber)"
-
-                message = (messages[messageNumber])
-                messageNumber += 1
-                if messageNumber == messages.count {
-                    messageNumber = 0
-                }
+                var messageNumber: Int
+                repeat  {
+                    messageNumber = Int.random(in: 0..<messages.count)
+                } while messageNumber == lastMessageNumber
+                
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
                 
                 //TODO: Update the imageName variable
-                imageNumber += 1
-                if imageNumber > 9  {
-                    imageNumber = 0
+                var imageNumber = Int.random(in: 0..<numberOfImages)
+                while imageNumber == lastImageNumber {
+                    imageNumber = Int.random(in: 0..<numberOfImages)
                 }
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
+                
+                repeat  {
+                    soundNumber = Int.random(in: 0..<numberOfSounds)
+                } while soundNumber == lastSoundNumber
+                
+                lastSoundNumber = soundNumber
+                let soundName = "sound\(soundNumber)"
+                guard let soundFile = NSDataAsset(name: soundName) else {
+                    print("😡 Could not read file named \(soundName)")
+                    return
+                }
+                do{
+                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                    audioPlayer.stop()
+                    audioPlayer.play()
+                } catch {
+                    print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+                }
+                
                 
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
+            
+            Toggle("Sound On", isOn: $soundOn)
+//            if soundOn{
+//                audioPlayer.play()
+//            } else {
+//                audioPlayer.stop()
+//            }
+        let _ = print($soundOn)
+
         }
-        
         .padding()
     }
     
