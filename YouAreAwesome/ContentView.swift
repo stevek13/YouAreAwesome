22373d1dad351358c41925650d1ba965c9603a23
Steve Kite <skite1@hotmail.com>
2026-08-21 05:03:21 -0700

    Added missing space

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("I ama developer!")
+            Text("I am a developer!")
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
