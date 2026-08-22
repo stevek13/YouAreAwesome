f2ed902f46f0e8f4fd41daddf6b7ac468b0c2e89
Steve Kite <skite1@hotmail.com>
2026-08-22 10:42:42 -0700

    After button variable and State

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message = "Stay Hungry. Stay Foolish!"
+    
     var body: some View {
         VStack {
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("I am a developer!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
-            Text("Stay Hungry. Stay Foolish.").fontWeight(.bold).font(.title)
+            Button {
+                message = "You're Awesome!"
+            } label: {
+                Text("Click Me!")
+            }
+
+//            Text("Stay Hungry. Stay Foolish.").fontWeight(.bold).font(.title)
         }
         .padding()
     }
